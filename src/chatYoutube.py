794954090261a13982fc4a84e0c4a02e940ef237
itsvins <itsvins63@gmail.com>
2024-09-24 09:58:49 +0530

    Update chatYoutube.py

--- src/chatYoutube.py
+++ src/chatYoutube.py
@@ -35,7 +35,7 @@ if youtube_url:
         vector_store = Chroma.from_documents(chunks, embeddings)
 
         # llm = OpenAI(temperature=0)
-        llm = ChatOpenAI(model="gpt-4o", temperature=1)
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
 
         retriever = vector_store.as_retriever()
 
@@ -45,27 +45,21 @@ if youtube_url:
         st.session_state['crc'] = crc
         st.success("Video Loaded successfully")
 
-questions = ["Summarise this video", "Who is the speaker?", "What is the main topic?, generate quesions based on the transcript"]
-selected_question = st.selectbox("Select a question:", questions)
-submit_button = st.button("Submit Question")
+question = st.text_input("Ask a question about the constitution")
 
-if submit_button:
+if question:
     # response = chain.run(question)
     if 'crc' in st.session_state:
         crc = st.session_state['crc']
         if 'history' not in st.session_state:
             st.session_state['history'] = []
         
-        response = crc.run({'question': selected_question, 'chat_history': st.session_state['history']})
+        response = crc.run({'question': question, 'chat_history': st.session_state['history']})
         
-        st.session_state['history'].append((selected_question, response))
+        st.session_state['history'].append((question, response))
         st.write(response)
 
         # st.write(st.session_state['history'])
         for prompts in st.session_state['history']:
             st.write("question: ", prompts[0])
             st.write("answer: ", prompts[1])
-
-            # Suggest the response as the next input
-        st.markdown("**Suggested next input:**")
-        st.markdown(f"`{response}`")
